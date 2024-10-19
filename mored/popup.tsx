6c4ae67bb019e03b51bd117f1a781ef1fcbde005
JaveRynx <raghavsharma0257@gmail.com>
2024-10-19 17:31:36 +0530

    Mored extension scaffolding

---
+++ mored/popup.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+
+const Popup = () => {
+    return (
+        <div className="popup-container">
+            <h2 className="popup-title">Mored</h2>
+        </div>
+    )
+}
+
+export default Popup
